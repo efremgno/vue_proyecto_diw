feedd226f77c1ccd9532706a07a9e9b9f4621721
efremgn <efremnogueiras@gmail.com>
2024-01-10 12:22:47 +0100

    Version 1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Feliz 2024"/>
+  <PrimerasPruebas mensaje="Primeras pruebas"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PrimerasPruebas from './components/PrimerasPruebas.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PrimerasPruebas
   }
 }
 </script>
---
+++ src/components/PrimerasPruebas.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hello">
+        <h1>{{ mensaje }}</h1>
+        <h3>Primeras pruebas</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PrimerasPruebas',
+    props: {
+        mensaje: String
+    }
+}
+
+
+
+</script>
+
+<style>
+
+
+</style>
