c3fe34761b165f15fb6cf7b512393b5155850823
efremgn <efremnogueiras@gmail.com>
2024-01-15 09:34:29 +0100

    Versión inicial

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Feliz 2024"/>
-  <PrimerasPruebas mensaje="Primeras pruebas"/>
+  <div><p>pruebas</p></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import PrimerasPruebas from './components/PrimerasPruebas.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    PrimerasPruebas
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
