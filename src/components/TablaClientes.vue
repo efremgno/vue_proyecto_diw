360cc8fd24298a19ad06c0312c5a0a9e31dc20d0
efremgno <146180826+efremgno@users.noreply.github.com>
2024-01-18 09:55:22 +0100

    Tercer commit

--- src/components/TablaClientes.vue
+++ src/components/TablaClientes.vue
@@ -3,29 +3,68 @@
 
         <nav class="navbar navbar-dark bg-primary">
             <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <a class="navbar-brand" href="#">Inicio</a>
                 <a class="navbar-brand" href="#">Clientes</a>
                 <a class="navbar-brand" href="#">Artículos</a>
                 <a class="navbar-brand" href="#">Ventas</a>
-                <form class="d-flex">
+                <div class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Búsqueda" aria-label="Search">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </form>
+                    <button class="btn btn-light" type="submit" data-v-f0802bba=""><i class="bi bi-search"
+                            data-v-f0802bba=""></i></button>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarToggleExternalContent" data-v-f0802bba="">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" data-v-f0802bba="">
+                        <li class="nav-item" data-v-f0802bba=""><a class="nav-link" href="#"
+                                data-v-f0802bba="">Articulos</a></li>
+                        <li class="nav-item" data-v-f0802bba=""><a class="nav-link" href="#" data-v-f0802bba="">Ventas</a>
+                        </li>
+                        <li class="nav-item" data-v-f0802bba=""><a class="nav-link" href="#" data-v-f0802bba="">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
+        <br>
         <div class="row">
-            <h2 class="text-center">Tabla de clientes</h2>
+            <h3 class="text-center font-weight-bold">Gestión clientes</h3>
         </div>
-        <table class="table table-striped">
-            <thead>
-                <tr>
+        <hr>
+        <!-- Formulario -->
+        <div class="container-fluid">
+            <div class="row" id="map_section">
+                <div class="col-12 col-m-6 col-lg-8 mx-auto">
+                    <form class="form-inline">
+                        <div class="input-group mb-3"><span class="input-group-text custom-span">DNI:</span><input
+                                type="text" class="form-control form-control-sm" id="dni" name="dni"></div>
+                        <div class="input-group mb-3"><span class="input-group-text custom-span">Nombre:</span><input
+                                type="text" class="form-control" id="nombre" name="nombre"></div>
+                        <div class="input-group mb-3"><span class="input-group-text custom-span">Apellido:</span><input
+                                type="text" class="form-control" id="apellido" name="apellido"></div>
+                        <div class="input-group mb-3"><span class="input-group-text custom-span">Email:</span><input
+                                type="text" class="form-control" id="email" name="email"></div>
+                        <div class="text-center"><button type="button" class="btn btn-primary m-2">Guardar</button><button
+                                type="button" class="btn btn-secondary">Limpiar</button></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <h5 class="text-center font-weight-bold">Listado Clientes</h5>
+        </div>
+        <table class="table table-striped  table-bordered">
+            <thead class="table-info">
+                <tr class="text-center">
                     <th>Nombre</th>
                     <th>Apellido</th>
                     <th>DNI</th>
                     <th>E-mail</th>
+                    <th>Acciones</th>
                 </tr>
             </thead>
             <tbody>
@@ -35,10 +74,8 @@
                     <td>{{ cliente.dni }}</td>
                     <td>{{ cliente.email }}</td>
                     <td>
-                        <button class="btn btn-warning">Editar</button>
-                    </td>
-                    <td>
-                        <button class="btn btn-danger">Eliminar</button>
+                        <button class="btn btn-warning"><i class="bi bi-pencil-square"></i></button>
+                        <button class="btn btn-danger"><i class="bi bi-trash3"></i></button>
                     </td>
                 </tr>
             </tbody>
