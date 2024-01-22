b2b5d3502087dc1d17c9bc6b227785a4e0d0dd33
efremgno <146180826+efremgno@users.noreply.github.com>
2024-01-22 09:49:22 +0100

    Agregar clientes

--- src/components/TablaClientes.vue
+++ src/components/TablaClientes.vue
@@ -40,15 +40,17 @@
                 <div class="col-12 col-m-6 col-lg-8 mx-auto">
                     <form class="form-inline">
                         <div class="input-group mb-3"><span class="input-group-text custom-span">DNI:</span><input
-                                type="text" class="form-control form-control-sm" id="dni" name="dni"></div>
+                                type="text" class="form-control form-control-sm" id="dni" name="dni" @blur="validarDniNie">
+                        </div>
                         <div class="input-group mb-3"><span class="input-group-text custom-span">Nombre:</span><input
                                 type="text" class="form-control" id="nombre" name="nombre"></div>
                         <div class="input-group mb-3"><span class="input-group-text custom-span">Apellido:</span><input
                                 type="text" class="form-control" id="apellido" name="apellido"></div>
                         <div class="input-group mb-3"><span class="input-group-text custom-span">Email:</span><input
                                 type="text" class="form-control" id="email" name="email"></div>
-                        <div class="text-center"><button type="button" class="btn btn-primary m-2">Guardar</button><button
-                                type="button" class="btn btn-secondary">Limpiar</button></div>
+                        <div class="text-center"><button type="button" class="btn btn-primary m-2"
+                                @click="guardar">Guardar</button><button type="button" class="btn btn-secondary"
+                                @click="limpiar">Limpiar</button></div>
                     </form>
                 </div>
             </div>
@@ -84,10 +86,15 @@
 </template>
 
 <script>
+import Swal from 'sweetalert2';
 export default {
     name: 'TablaClientes',
     data() {
         return {
+            nombre: '',
+            apellido: '',
+            dni: '',
+            email: '',
             clientes: [
                 {
                     id: 1,
@@ -112,8 +119,140 @@ export default {
                 }
             ]
         }
+    },
+    methods: {
+        guardar() {
+            // Controlar los campos que están vacíos
+            if (this.nombre.trim() === '' || this.apellido.trim() === '' || this.dni.trim() === '' || this.email.trim() === '') {
+                this.mostrarAlerta('Debe Completar los Campos', 'warning');
+            } else {
+                // Lógica para guardar los datos del formulario
+                // Crear un nuevo cliente con los datos del usuario
+                const nuevoCliente = {
+                    id: this.clientes.length + 1, // asignar un nuevo ID
+                    nombre: this.nombre.trim().toUpperCase(),
+                    apellido: this.apellido.trim().toUpperCase(),
+                    dni: this.dni.trim().toUpperCase(),
+                    email: this.email.trim(),
+                };
+
+                // Agregar el nuevo cliente al array clientes
+                this.clientes.push(nuevoCliente)
+
+                // Restablecer los datos del formulario
+                this.nombre = nuevoCliente.nombre;
+                this.apellido = nuevoCliente.apellido;
+                this.dni = nuevoCliente.dni;
+                this.email = nuevoCliente, this.email;
+
+                // Mostrar alerta de éxito
+                this.mostrarAlerta('Cliente guardado correctamente', 'success');
+            }
+        },
+
+        // Limpiar campos del formulario
+        limpiar() {
+            // Lógica para limpiar los campos del formulario
+            this.nombre = '';
+            this.apellido = '';
+            this.dni = '';
+            this.email = '';
+
+            this.mostrarAlerta('Campos limpiados', 'info')
+            return;
+        },
+
+        // Validar DNI o NIE
+        validarDniNie() {
+            const dniNie = this.dni.trim().toUpperCase(); // Convierte a mayúsculas para simplificar la validación
+            this.dni = dniNie; // Actualiza el valor del campo
+            // Expresión regular para validar DNI y NIE
+            const regexDniNie = /^[0-9XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKE]$/;
+
+            if (!regexDniNie.test(dniNie)) {
+                this.mostrarAlerta('DNI o NIE no válido', 'error');
+                this.dni = ''; // Limpiar el campo si el DNI o NIE no es válido
+                return;
+            }
+            // Validar el dígito de control
+            const numero = parseInt(dniNie.slice(0, 8), 10);
+            let letraCalculada = '';
+
+            // Verificar si es un NIE (XYZ)
+            if (dniNie.charAt(0) === 'X' || dniNie.charAt(0) === 'Y' || dniNie.charAt(0) === 'Z') {
+                letraCalculada = 'TRWAGMYFPDXBNJZSQVHLCKE'.charAt(numero % 23);
+            } else {
+                letraCalculada = 'TRWAGMYFPDXBNJZSQVHLCKE'.charAt(numero % 23);
+            }
+
+            if (letraCalculada !== dniNie.charAt(8)) {
+                this.mostrarAlerta('DNI o NIE no válido', 'error');
+                this.dni = ''; // Limpiar el campo si el DNI no es válido
+            }
+        },
+        // Mostrar ventana alerta
+        mostrarAlerta(mensaje, tipo) {
+            Swal.fire({
+                title: mensaje,
+                icon: tipo,
+                customClass: {
+                    container: 'custom-alert-container',
+                    popup: 'custom-alert',
+                    confirmButton: 'custom-alert-button',
+                }
+            })
+        },
+        // Eliminar cliente
+        eliminarCliente(clienteId) {
+            // Encontrar el código
+            const index = this.clientes.findIndex(cliente => cliente.id === clienteId);
+
+            // Comprobar si el cliente existe
+            if (index !== -1) {
+                // borrado del cliente
+                this.clientes.splice(index, 1);
+
+                // mostrando alerta de éxito
+                this.mostrarAlerta('Cliente eliminado correctamente', 'succes');
+            } else {
+                // mostrar alerta de error si el cliente no existe
+                this.mostrarAlerta('Cliente no econtrado', 'error');
+            }
+        },
+        modificarCliente(clienteId) {
+            // buscar el cliente con el ID dado
+            const cliente = this.clientes.find(cliente => cliente.id === clienteId);
+
+            // comprobar si el cliente existe 
+            if (cliente) {
+                // actualizar los campos del formulario con los datos del cliente
+                this.nombre = cliente.nombre;
+                this.apellido = cliente.apellido;
+                this.dni = cliente.dni;
+                this.email = cliente.email;
+
+                // mostrar alerta de éxito
+                this.mostrarAlerta('Datos del cliente listos para modificar', 'info');
+            } else {
+                //mostrar alerta de error si el cliente no existe
+                this.mostrarAlerta('Cliente no encontrado', 'error');
+            }
+        },
     }
 }
 </script>
 
-<style></style>
+<style>
+    .btn-no-hover:hover { /* no cambie de color */
+        background-color: white !important;
+        border-color: white !important;
+    }
+    .btno-no-hover i {
+        color: #000000 !important;
+    }
+    .custom-span {
+        width: 120px;
+        display: inline-block;
+        text-align: left;
+    }
+</style>
