de644636916eada23476697b60b1606937ed07d3
Efrem García Nogueiras <a20efremgn@ateis.local>
2024-02-05 09:45:45 +0100

    Add: MongoDB get y post

--- src/router/tareas.mjs
+++ src/router/tareas.mjs
@@ -1,11 +1,19 @@
 import express from 'express';
 const rutas = express.Router();
 
+import tarea from '../models/tarea.mjs';
+
 // otra forma de crear rutas
 
-rutas.get('/', (req, res) => {
-    res.send('Ruta de las tareas');
+rutas.get('/', async(req, res) => {
+    const tareas = await tarea.find();
+    res.json(tareas);
 });
 
+router.post('/', async(req, res) => {
+    new tarea()
+    console.log(new tarea())
+})
 
-export default rutas;
+model.exports = rutas;
+//export default rutas;
--- src/server.mjs
+++ src/server.mjs
@@ -4,6 +4,7 @@
 import express from 'express';
 import http from 'http';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 import tareasRouter from './router/tareas.mjs'
 
 const app = express()
@@ -29,3 +30,6 @@ app.get('/', (req, res) => {
 server.listen(app.get('port'), () => {
     console.log('Servidor corriendo en el puerto...', app.get('port'));
 });
+mongoose.connect('mongodb://localhost:27017/bbdd')
+    .then(db => console.log('Conectado a la base de datos'))
+    .catch(error => console.log('Error al conectar a la base de datos', error))
