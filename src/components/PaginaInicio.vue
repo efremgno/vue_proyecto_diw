f560594a4b928eaa5550e10614b861709dc534ec
efremgno <efremnogueiras@gmail.com>
2024-03-06 12:52:56 +0100

    Pagina de inicio actualizada

--- src/components/PaginaInicio.vue
+++ src/components/PaginaInicio.vue
@@ -1,9 +1,35 @@
 <template>
     <div>
-        <NavBar/>
+        <NavBar />
     </div>
+    <br>
     <div>
-        <h1 class="text-center">Página proyecto del {{ alumno_a }}</h1>
+        <h1 class="text-center">Página proyecto de {{ alumno_a }}</h1>
+        <div class="container text-center mt-5">
+            <div class="row mx-auto">
+                <div class="col">
+                    <router-link to="/clientes" class="navbar-brand">
+                        <button type="button" class="btn btn-primary btn-lg m-3 p-5 w-25">
+                            Gestión Cliente
+                        </button></router-link>
+                    <router-link to="/tareas" class="navbar-brand">
+                        <button type="button" class="btn btn-primary btn-lg m-3 p-5 w-25">
+                            Gestión Tareas
+                        </button>
+                    </router-link>
+                </div>
+            </div>
+            <div class="row mx-auto">
+                <div class="col">
+                    <router-link to="/articulos" class="navbar-brand">
+                        <button type="button" class="btn btn-primary btn-lg m-3 p-5 w-25">
+                            Gestión Artículos
+                        </button>
+                    </router-link>
+                    <button type="button" class="btn btn-primary btn-lg m-3 p-5 w-25">Gestión ventas</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -17,7 +43,7 @@ export default {
     },
     data() {
         return {
-            alumno_a: 'nombre del alumno/a'
+            alumno_a: 'Efrem'
         }
     },
     mounted() {
@@ -27,6 +53,4 @@ export default {
 </script>
 
 
-<style>
-
-</style>
+<style></style>
